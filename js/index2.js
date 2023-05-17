5b694097f49dce96cc0fddb44c52c5c3fdfedbc1
Tran_Vuong <trankimvuong25@gmail.com>
2023-05-17 10:39:42 +0700

    Fix idex 2

--- js/index2.js
+++ js/index2.js
@@ -8,9 +8,15 @@
  * Nếu kw > 50 & kw <= 100: *650
  * Nếu kw > 100 & kw <= 200: *850
  * Nếu kw > 200 & kw <= 350: *1100
- * Nếu kw > 250: *1300
+ * Nếu kw > 350: *1300
 
  */
+const KW_DUOI_50 = 500
+const KW_50_100 = 650
+const KW_100_200 = 850 
+const KW_200_350 = 1100
+const KW_TREN_350 = 1300
+
 
 function calcPrice() {
     var name = document.getElementById("inpName").value;
@@ -19,15 +25,15 @@ function calcPrice() {
     var totalPrice =0
 
     if (num <= 50){
-        totalPrice = num * 500
+        totalPrice = num * KW_DUOI_50
     } else if (num > 50 && num <= 100){
-        totalPrice = (50 * 500) + ((num - 50) * 650) 
+        totalPrice = (50 * KW_DUOI_50) + ((num - 50) * KW_50_100) 
     } else if (num > 100 && num <= 200){
-        totalPrice = (50 * 500) + (50 * 650) + ((num - 100) * 850) 
+        totalPrice = (50 * KW_DUOI_50) + (50 * KW_50_100) + ((num - 100) * KW_100_200) 
     } else if (num > 200 && num <= 350){
-        totalPrice = (50 * 500) + (50 * 650) + (100 * 850) ((num - 200) * 1100) 
+        totalPrice = (50 * KW_DUOI_50) + (50 * KW_100_200) + (100 * KW_100_200) + ((num - 200) * KW_200_350) 
     } else {
-        totalPrice = (50 * 500) + (50 * 650) + (100 * 850) (150 * 1100) + ((num - 350) * 1300)
+        totalPrice = (50 * KW_DUOI_50) + (50 * KW_50_100) + (100 * KW_100_200) + (150 * KW_200_350) + ((num - 350) * KW_TREN_350)
     }
 
     document.getElementById("txtNotify").innerHTML = "Tên chủ hộ: " + name + "<br>Tổng tiền điện: " + totalPrice.toLocaleString()
